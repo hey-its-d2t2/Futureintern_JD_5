30383e26653efba772e01e803948c4a0eb4b3c12
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-03 16:51:02 +0530

    Initial commit: Application created with some dependencies like Bpringboot Web, Lombok,Thymeleaf, DevTools form start.spring.io

---
+++ src/main/java/com/Futureintern_JD_5/FutureinternJd5Application.java
@@ -0,0 +1,13 @@
+package com.Futureintern_JD_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureinternJd5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureinternJd5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Futureintern_JD_5/FutureinternJd5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Futureintern_JD_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureinternJd5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
