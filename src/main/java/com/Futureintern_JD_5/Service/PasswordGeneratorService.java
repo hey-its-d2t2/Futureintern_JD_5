0ec08996c324a41816464a45ecfd6098fae3edb3
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-06 08:48:14 +0530

    Generate password service class updated., following MVC Architecture and some code removed and implemented in Service Impl class.

--- src/main/java/com/Futureintern_JD_5/Service/PasswordGeneratorService.java
+++ src/main/java/com/Futureintern_JD_5/Service/PasswordGeneratorService.java
@@ -3,31 +3,18 @@ package com.Futureintern_JD_5.Service;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
+import java.util.List;
 
-@Service
-public class PasswordGeneratorService {
+public  interface PasswordGeneratorService {
+     List<String> generatePasswords(int length, int quantity,
+                                            boolean includeNumbers, boolean includeLowercase,
+                                            boolean includeUppercase, boolean includeSymbols,
+                                            boolean noSimilarChars, boolean noDuplicateChars,
+                                            boolean noSequentialChars);
 
-    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
-    private static final String DIGITS = "0123456789";
-    private static final String SPECIAL_CHARACTERS = "!@#$%^&*()-_+=<>?";
-
-    public String generatePassword(int length, boolean useUpper, boolean useLower, boolean useDigits, boolean useSpecial) {
-        StringBuilder characterPool = new StringBuilder();
-        SecureRandom random = new SecureRandom();
-
-        if (useUpper) characterPool.append(UPPERCASE);
-        if (useLower) characterPool.append(LOWERCASE);
-        if (useDigits) characterPool.append(DIGITS);
-        if (useSpecial) characterPool.append(SPECIAL_CHARACTERS);
-
-        if (characterPool.length() == 0) throw new IllegalArgumentException("No character types selected");
-
-        StringBuilder password = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
-            int index = random.nextInt(characterPool.length());
-            password.append(characterPool.charAt(index));
-        }
-        return password.toString();
-    }
+     String generatePassword(int length, boolean includeNumbers,
+                                     boolean includeLowercase, boolean includeUppercase,
+                                     boolean includeSymbols, boolean noSimilarChars,
+                                     boolean noDuplicateChars, boolean noSequentialChars);
 }
+
