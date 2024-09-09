1095f010be6c010a025adc145e1f85794a6bc429
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-09 09:44:57 +0530

    Controller,context path updated.

--- src/main/java/com/Futureintern_JD_5/Controller/PasswordGeneratorController.java
+++ src/main/java/com/Futureintern_JD_5/Controller/PasswordGeneratorController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @Controller
-@RequestMapping
+@RequestMapping("/password-generator")
 public class PasswordGeneratorController {
 
     private final PasswordGeneratorService passwordGeneratorService;
