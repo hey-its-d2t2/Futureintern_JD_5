38872a1a6a747a3d6cfc75d8aaff53a540b482d9
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-13 13:57:24 +0530

    Final Commit.

--- src/main/java/com/Futureintern_JD_5/Controller/PasswordGeneratorController.java
+++ src/main/java/com/Futureintern_JD_5/Controller/PasswordGeneratorController.java
@@ -4,12 +4,9 @@ import com.Futureintern_JD_5.Service.PasswordGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import org.springframework.web.bind.annotation.*;
-import java.util.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/password-generator")
@@ -22,13 +19,14 @@ public class PasswordGeneratorController {
         this.passwordGeneratorService = passwordGeneratorService;
     }
 
-
+    // Serve the password generator page
     @GetMapping
     public String showPasswordGeneratorPage() {
         return "passwordgenerator";
     }
 
-    @PostMapping("/password-generate")
+    // Generate passwords based on the user's input
+    @PostMapping("/generate")
     public String generatePasswords(@RequestParam("length") int length,
                                     @RequestParam("quantity") int quantity,
                                     @RequestParam(value = "includeNumbers", required = false, defaultValue = "false") boolean includeNumbers,
@@ -42,14 +40,13 @@ public class PasswordGeneratorController {
 
         // Generate the passwords using the service
         List<String> passwords = passwordGeneratorService.generatePasswords(length, quantity, includeNumbers,
-                includeLowercase, includeUppercase,
-                includeSymbols, noSimilarChars,
-                noDuplicateChars, noSequentialChars);
+                includeLowercase, includeUppercase, includeSymbols,
+                noSimilarChars, noDuplicateChars, noSequentialChars);
 
         // Add generated passwords to the model to pass them to the view
         model.addAttribute("passwords", passwords);
 
-        // Return the same HTML page, but now with passwords displayed
+        // Return the same page with the generated passwords displayed
         return "passwordgenerator";
     }
 
